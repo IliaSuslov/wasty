26a8003038a8c76eb32ac4892020a84af38c5e78
alexsuslov <suslov@me.com>
2020-08-31 20:18:53 +0300

    home handle

---
+++ internal/hands/home.go
@@ -0,0 +1,19 @@
+package hands
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(DB *mongo.Database, t *template.Template)func(w http.ResponseWriter, r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request){
+		log.Println("home")
+		//w.WriteHeader(200)
+		err:=t.ExecuteTemplate(w, "index.html", struct{Title string}{"HOME"})
+		if err!= nil{
+			log.Println(err)
+		}
+	}
+}
