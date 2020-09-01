eec43f2f5ab977d5c79f9e8f645594f20c273a3a
alexsuslov <suslov@me.com>
2020-09-01 18:35:44 +0300

    user is role

--- api/model/user.go
+++ api/model/user.go
@@ -11,49 +11,60 @@ import (
 	"log"
 )
 
-type User struct{
-	ID primitive.ObjectID
-	Name string
+type User struct {
+	ID       primitive.ObjectID
+	Name     string
 	Fullname string
 	Password [32]byte
-	Roles []string
-	Email string
-	QRcode string
+	Roles    []string
+	Email    string
+	QRcode   string
 }
 
-type Role struct{
-	ID primitive.ObjectID
+func (User *User) IsRole(roles ...string) bool {
+	for _, r := range User.Roles {
+		for _, role := range roles {
+			if r == role {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type Role struct {
+	ID   primitive.ObjectID
 	Name string
 }
 
-func CreateAdmin(DB *mongo.Database)error{
+func CreateAdmin(DB *mongo.Database) error {
 	col := DB.Collection("users")
-	res:=col.FindOne(context.Background(), bson.M{"name":"admin"})
-	if res.Err()==nil{
+	res := col.FindOne(context.Background(), bson.M{"name": "admin"})
+	if res.Err() == nil {
 		return nil
 	}
 	sid, _ := shortid.New(1, shortid.DefaultABC, 2342)
 	password, _ := sid.Generate()
 	hash := sha256.Sum256([]byte(password))
 	str := fmt.Sprintf("%x", hash)
-	hash = sha256.Sum256([]byte(str+"admin"))
+	hash = sha256.Sum256([]byte(str + "admin"))
 	log.Printf("create user admin pass=%v", password)
-	_, err :=col.InsertOne(context.Background(), &User{
-		Name: "admin",
+	_, err := col.InsertOne(context.Background(), &User{
+		Name:     "admin",
 		Password: hash,
-		Roles: []string{"admin"},
+		Roles:    []string{"admin"},
 	})
 	return err
 }
 
-func NewUser(Name string)*User{
+func NewUser(Name string) *User {
 	return &User{Name: Name}
 }
 
-func IsRole(roles interface{}, role string)int{
+func IsRole(roles interface{}, role string) int {
 	Roles := roles.([]string)
-	for i, v := range Roles{
-		if v ==role{
+	for i, v := range Roles {
+		if v == role {
 			return i
 		}
 	}
