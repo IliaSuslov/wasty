60acb305dcae618ce24e4ce986d37e230556ef51
alexsuslov <suslov@me.com>
2020-09-01 15:58:46 +0300

    address query

--- internal/hands/addresses.go
+++ internal/hands/addresses.go
@@ -6,36 +6,82 @@ import (
 	"github.com/alexsuslov/wasty/api/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
+	"strings"
 )
 
-type AddressQuery struct{
-	Name *string
-	Limit *int
-	Offset *int
+func GetOpts(sort string, limit *int64, skip *int64) (opt *options.FindOptions) {
+	opt = &options.FindOptions{
+		Limit: limit,
+		Skip:  skip,
+	}
+
+	if strings.HasPrefix(sort, "!") {
+		opt.Sort = bson.M{sort[1:]: -1}
+	} else {
+		opt.Sort = bson.M{sort: 1}
+	}
+	return
+}
+
+func OnError(w http.ResponseWriter, err error) bool {
+	if err == nil {
+		return false
+	}
+	w.WriteHeader(500)
+	err = json.NewEncoder(w).Encode(struct {
+		Success bool
+		Message string
+	}{
+		false,
+		err.Error(),
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	return true
 }
 
 func Addresses(DB *mongo.Database) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		//todo: check permishion
-		query:=bson.M{}
+
+		//query
+		q := &struct {
+			Name  *string
+			Skip  *int64
+			Limit *int64
+		}{}
+		json.NewDecoder(r.Body).Decode(&q)
+		defer r.Body.Close()
+		//name
+		query := bson.M{}
+		if q.Name != nil {
+			query["name"] = bson.M{"$regex": q.Name}
+		}
+
 		c := DB.Collection("addresses")
 		w.Header().Set("ContentType", "application/json")
-		//todo: limit, skip
-		cur, err := c.Find(context.Background(), query)
-		if err!= nil{
-			log.Println(err)
+		opt := &options.FindOptions{
+			Limit: q.Limit,
+			Skip:  q.Skip,
 		}
-		for cur.Next(context.Background()){
+		cur, err := c.Find(context.Background(), query, opt)
+		if OnError(w, err) {
+			return
+		}
+		for cur.Next(context.Background()) {
 			Addr := &model.Address{}
-			err :=cur.Decode(Addr)
-			if err!= nil{
-				log.Println(err)
+			err := cur.Decode(Addr)
+			if OnError(w, err) {
+				return
 			}
-			err=json.NewEncoder(w).Encode(Addr)
-			if err!= nil{
-				log.Println(err)
+			json.NewEncoder(w).Encode(Addr)
+			if OnError(w, err) {
+				return
 			}
 		}
 	}
