adbe14e4a240ec00fa85509f02f9459f515909ec
alexsuslov <suslov@me.com>
2020-09-02 07:19:07 +0300

    pages 500 401

---
+++ internal/hands/500.go
@@ -0,0 +1,25 @@
+package hands
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func OnError(w http.ResponseWriter, err error) bool {
+	if err == nil {
+		return false
+	}
+	w.WriteHeader(500)
+	err = json.NewEncoder(w).Encode(struct {
+		Success bool
+		Message string
+	}{
+		false,
+		err.Error(),
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	return true
+}
