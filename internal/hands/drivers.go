e3834938098dbfbefec5304c43f1ba599d7e8a57
alexsuslov <suslov@me.com>
2020-09-02 07:20:30 +0300

    driver

--- internal/hands/drivers.go
+++ internal/hands/drivers.go
@@ -7,7 +7,6 @@ import (
 	gcontext "github.com/gorilla/context"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"log"
 	"net/http"
 )
 
@@ -21,24 +20,28 @@ func Drivers(DB *mongo.Database) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user := gcontext.Get(r, "user")
 		User := user.(model.User)
-		if !User.IsRole("admin", "manager"){
+		if !User.IsRole("admin", "manager") {
 			Denied(w)
 			return
 		}
+		//Query
+		q := r.URL.Query()
 		query := bson.M{}
+		qregex := AddLowerRegex(query, q)
+		qregex("name")
+
 		drivers := DB.Collection("drivers")
 		w.Header().Set("ContentType", "application/json")
-		//todo: limit, skip
-		cur, err := drivers.Find(context.Background(), query)
-		if err != nil {
-			log.Println(err)
+		cur, err := drivers.Find(context.Background(), query, GetOpts(q))
+		if OnError(w, err) {
+			return
 		}
 		res := []*model.Driver{}
 		for cur.Next(context.Background()) {
 			Driver := &model.Driver{}
 			err := cur.Decode(Driver)
-			if err != nil {
-				log.Println(err)
+			if OnError(w, err) {
+				return
 			}
 			res = append(res, Driver)
 		}
