e7dc040635c243355dbbeda9a3b10bc9e5e5164a
Alex Suslov <suslov@me.com>
2020-09-07 11:07:54 +0300

    car

---
+++ api/car/model.go
@@ -0,0 +1,42 @@
+
+package car
+
+import (
+  "fmt"
+  "go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Car struct{
+    ID primitive.ObjectID `bson:"_id,omitempty"` 
+    Name string `bson:"name"` 
+    Number string `bson:"number"` 
+    Trailer string `bson:"trailer"` 
+  echo string
+}
+
+func New()*Car{
+  return &Car{}
+}
+func (Car *Car)SetID(ID primitive.ObjectID)*Car {
+    Car.echo = fmt.Sprintf("set ID from %v to %v",  Car.ID, ID)
+    Car.ID=ID
+    return Car
+}
+func (Car *Car)SetName(Name string)*Car {
+    Car.echo = fmt.Sprintf("set Name from %v to %v",  Car.Name, Name)
+    Car.Name=Name
+    return Car
+}
+func (Car *Car)SetNumber(Number string)*Car {
+    Car.echo = fmt.Sprintf("set Number from %v to %v",  Car.Number, Number)
+    Car.Number=Number
+    return Car
+}
+func (Car *Car)SetTrailer(Trailer string)*Car {
+    Car.echo = fmt.Sprintf("set Trailer from %v to %v",  Car.Trailer, Trailer)
+    Car.Trailer=Trailer
+    return Car
+}
+func (Car *Car)GetEcho()string {
+    return Car.echo
+}
