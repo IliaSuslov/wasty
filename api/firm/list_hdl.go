cf6a22899ddadaa7392b747622ac95ebe4a28e00
Alex Suslov <suslov@me.com>
2020-09-07 11:08:34 +0300

    firm

---
+++ api/firm/list_hdl.go
@@ -0,0 +1,87 @@
+
+package firm
+
+import (
+	"context"
+	"encoding/json"
+	gcontext "github.com/gorilla/context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+
+func Atoin64(s string, def int64) *int64 {
+	if s ==""{
+		return &def
+	}
+	i, _ := strconv.Atoi(s)
+	I := int64(i)
+	return &I
+}
+
+
+func GetOpts(values url.Values) (opt *options.FindOptions) {
+	sort := values.Get("sort")
+	limit := values.Get("limit")
+	skip := values.Get("skip")
+
+	opt = &options.FindOptions{
+		Limit: Atoin64(limit, 100),
+		Skip:  Atoin64(skip, 0),
+	}
+	if sort!= ""{
+		if strings.HasPrefix(sort, "!") {
+			opt.Sort = bson.M{sort[1:]: -1}
+		} else {
+			opt.Sort = bson.M{sort: 1}
+		}
+	}
+	return
+}
+
+
+func ListHDL(Col *mongo.Collection,
+	IsAllow func(User interface{}) bool, Denied func(w http.ResponseWriter),
+	OnError func(http.ResponseWriter, error) bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := gcontext.Get(r, "user")
+		if !IsAllow(user) {
+			Denied(w)
+			return
+		}
+		q := r.URL.Query()
+		query := bson.M{}
+    
+    
+		
+		Name:=q.Get("Name")
+		if Name!= ""{
+		query[strings.ToLower("Name")] = bson.M{"$regex": Name}
+		}
+    
+
+		cur, err := Col.Find(context.Background(), query, GetOpts(q))
+		if OnError(w, err) {
+			return
+		}
+		vals := []*Firm{}
+		w.Header().Set("ContentType", "application/json")
+		for cur.Next(context.Background()) {
+			val := &Firm{}
+			err := cur.Decode(val)
+			if OnError(w, err) {
+				return
+			}
+			vals = append(vals, val)
+		}
+		json.NewEncoder(w).Encode(vals)
+		if OnError(w, err) {
+			return
+		}
+	}
+}
---
+++ api/firm/model.go
@@ -0,0 +1,42 @@
+
+package firm
+
+import (
+  "fmt"
+  "go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Firm struct{
+    ID primitive.ObjectID `bson:"_id,omitempty"` 
+    Name string `bson:"name"` 
+    Contact string `bson:"contact"` 
+    Users []interface{} `bson:"users"` 
+  echo string
+}
+
+func New()*Firm{
+  return &Firm{}
+}
+func (Firm *Firm)SetID(ID primitive.ObjectID)*Firm {
+    Firm.echo = fmt.Sprintf("set ID from %v to %v",  Firm.ID, ID)
+    Firm.ID=ID
+    return Firm
+}
+func (Firm *Firm)SetName(Name string)*Firm {
+    Firm.echo = fmt.Sprintf("set Name from %v to %v",  Firm.Name, Name)
+    Firm.Name=Name
+    return Firm
+}
+func (Firm *Firm)SetContact(Contact string)*Firm {
+    Firm.echo = fmt.Sprintf("set Contact from %v to %v",  Firm.Contact, Contact)
+    Firm.Contact=Contact
+    return Firm
+}
+func (Firm *Firm)SetUsers(Users []interface{})*Firm {
+    Firm.echo = fmt.Sprintf("set Users from %v to %v",  Firm.Users, Users)
+    Firm.Users=Users
+    return Firm
+}
+func (Firm *Firm)GetEcho()string {
+    return Firm.echo
+}
